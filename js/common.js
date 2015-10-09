6c0272b62001491ebddbd7287f2fa9397dfb5704
natinavas <nnavas@itba.edu.ar>
2015-10-09 20:18:42 -0300

    cambio de javascript

---
+++ js/common.js
@@ -0,0 +1,8 @@
+(function(){
+	var app = angular.module('breadcrumbs', []);
+	app.controller('StoreController', function(){
+		this.first="HOMBRES"
+		this.second="REMERAS"
+		this.third="remera"
+	})
+})
