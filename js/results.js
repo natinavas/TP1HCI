fc8a625d9350dd10d9ced6a124a2a787a7a95c9d
fbartolome <fbartolome18@gmail.com>
2015-10-11 19:38:46 -0300

    cargando...

--- js/results.js
+++ js/results.js
@@ -12,14 +12,15 @@ loadMore();
 
 function loadMore(){
 
+		document.getElementById('loadMoreButton').innerHTML = "Cargando...";	
+
 		var request = new Object();
 		request.timeout = 7000;
 		request.url="http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetProductsByName&name="+input+"&page_size=24"+"&page=" + ++pageNum;
 		request.dataType="jsonp";
 		console.log(request.url);
-		$.ajax(request).done( function(data) {
 
-		//haciendo cagadas
+		$.ajax(request).done( function(data) {
 
 		var i  = 0;
 		for (i = 0; i < data.total; i++) { 
@@ -59,16 +60,15 @@ function loadMore(){
 
 			if(i % 4 == 3){
 
-			document.getElementById('products').innerHTML += '</tr>';
+				document.getElementById('products').innerHTML += '</tr>';
 			}
-
-
+			
 		}
 
-
+	
 
 	});
 
-
-
+	
+	document.getElementById('loadMoreButton').innerHTML = "CARGAR MAS";
 }
