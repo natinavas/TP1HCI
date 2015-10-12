e43e98f8e4252a79d0533731a460eb5b0ebcec54
fbartolome <fbartolome18@gmail.com>
2015-10-12 11:51:16 -0300

    carga filtros

--- js/filtros.js
+++ js/filtros.js
@@ -1,7 +1,41 @@
-var options = {
-  colors:[['#000000', '#424242', '#636363', '#9C9C94', '#CEC6CE', '#EFEFEF', '#EFF7F7', '#FFFFFF']]
-}
-$('#colorpalette4').colorPalette(options)
-  .on('selectColor', function(e) {
-    $('#selected-color3').val(e.color);
-  });
+var request = new Object();
+request.timeout = 7000;
+
+//Subo colores
+
+request.url="http://eiffel.itba.edu.ar/hci/service3/Common.groovy?method=GetAttributeById&id=4";
+request.dataType="jsonp";
+console.log(request.url);
+
+$.ajax(request).done( function(data){
+	var i = 0;
+	for(i =0 ; i < data.attribute.values.length; i++){
+		document.getElementById('color' + JSON.stringify(i+1)).innerHTML =  data.attribute.values[i];
+	}
+});
+
+//Subo marcas
+
+request.url="http://eiffel.itba.edu.ar/hci/service3/Common.groovy?method=GetAttributeById&id=9";
+request.dataType="jsonp";
+console.log(request.url);
+
+$.ajax(request).done( function(data){
+	var i = 0;
+	for(i =0 ; i < data.attribute.values.length; i++){
+		document.getElementById('trademark' + JSON.stringify(i+1)).innerHTML =  data.attribute.values[i];
+	}
+});
+
+//Subo ocasion
+
+request.url="http://eiffel.itba.edu.ar/hci/service3/Common.groovy?method=GetAttributeById&id=3";
+request.dataType="jsonp";
+console.log(request.url);
+
+$.ajax(request).done( function(data){
+	var i = 0;
+	for(i =0 ; i < data.attribute.values.length; i++){
+		document.getElementById('ocassion' + JSON.stringify(i+1)).innerHTML =  data.attribute.values[i];
+	}
+});
