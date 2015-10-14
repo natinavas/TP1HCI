c3bf27e8fcd9ab4dbbebd094632b4ae333b4fb24
natinavas <nnavas@itba.edu.ar>
2015-10-14 12:37:24 -0300

    cambios

--- js/navBarMenus.js
+++ js/navBarMenus.js
@@ -1,6 +1,6 @@
 
 var request = new Object();
-request.url = "http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetCategoryById&id=1";
+request.url = "http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetAllSubcategories&id=1";
 request.dataType = "jsonp";
 
 console.log(request.url);
@@ -8,17 +8,13 @@ console.log(request.url);
 $.ajax(request).done(function(calzado) {
 	error=calzado.error;
     if (error === undefined) {
-		alert(JSON.stringify(calzado.category.attributes[1].values[0]));
-		var mujer;
-		var hombre;
-		var bebe;
+		alert(calzado.subcategories[0].attributes[0].values[0]);
 		var i;
-		for(i=0; i<3;i++){
-			if(calzado.category.attributes[0].values[i] == "Adulto" && calzado.category.attributes[1].values[i] == "Femenino")
-				mujer=true;
-			if(calzado.category.attributes[0].values[i] == "Adulto" && calzado.category.attributes[1].values[i] == "Masculino")
-				hombre=true;
-			if(calzado.category.attributes[0].values[i] == "")
+		for(i=0; calzado.subcategorioes[i] != undefined; i++){
+			if(calzado.subcategories[i].attributes[0].values[0] == "Adulto" && calzado.category.attributes[1].values[i] == "Femenino"){
+				document.getElementById('calzadoMujeres').innerHTML = <li><a href="#">Alpargatas</a></li>;
+				
+			}
 	}			
 				
         document.getElementById('navBarMujeres').innerHTML += "";
