b6ca23a0cefc8f4d5e1334c24b8ac995340b27f8
gcuervo <cuervogaston@gmail.com>
2015-10-14 15:39:33 -0300

    angular y header

--- js/header.js
+++ js/header.js
@@ -1,145 +1,147 @@
-if (notSignedIn()) {
-    $("#navbarContent").append(notLoggedInButtons());
-    document.getElementById("sign-in").addEventListener("click", function() {
-        signIn()
-    });
-    document.getElementById("registrarse").addEventListener("click", function() {
-        CreateAccount()
-    });
-} else {
-    document.getElementById("cerrarSesion").addEventListener("click", function() {
-        signOut()
-    });
-    $("#navbarContent").append(loggedInButtons());
-}
+var app = angular.module('principal', []);   // esto es para utilizar $http.get()
 
+app.controller('PrincipalController', function($scope, $http, $log, $timeout) {  // el controller de angular
 
-
-function notSignedIn() {
-    var loggedUser = sessionStorage.getItem("loggedUser");
-    if (loggedUser == null || loggedUser == "" || loggedUser === "undefined" || loggedUser == "null" || loggedUser === undefined) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function notLoggedInButtons() {
-    var buttons = '<li><a href="#" data-toggle="modal" data-target="#myModal" >Registrarse</a></li>' + '<li class="divider-vertical"></li>' + '<li class="dropdown">' + '<a class="dropdown-toggle" href="#" data-toggle="dropdown">Iniciar Sesión<strong class="caret"></strong></a>' + '<div class="dropdown-menu" style="padding: 15px; padding-bottom: 9px;">' + '<form method="post"  accept-charset="UTF-8">' + '<input style="margin-bottom: 15px;" type="text" placeholder="Usuario" id="loginUser" name="username">' + '<input style="margin-bottom: 15px;" type="password" placeholder="Contraseña" id="loginPassword" name="password">' + '<input class="btn btn-primary btn-block" id="sign-in" value="Iniciar Sesión">' + '<a data-toggle="modal" data-target="#cont" href="#"><span class="olvidoC">¿Olvidaste tu contraseña?</span></a>' + '</form>' + '</div>' + '</li>';
-    return buttons;
-}
-
-function loggedInButtons() {
-    var buttons = ' <li class="dropdown">' + '<a class="dropdown-toggle" href="#" data-toggle="dropdown">Mi Cuenta<strong class="caret"></strong></a>' + '<div class="dropdown-menu" style="padding: 15px; padding-bottom: 9px;">' + '<form method="post" action="login" accept-charset="UTF-8">' + '<li><a href="usuario.html"><i class="glyphicon glyphicon-user" style = "color:black"></i><h7 style = "color:black">Mi Usuario</h7></a></li>' + '<li><a href="contacto.html"><i class="glyphicon glyphicon-envelope" style = "color:black"></i><h7 style = "color:black"> Contacto</h7></a></li>' + '<li class="divider"></li>' + '<li><a href="#" id="cerrarSesion"><i class="glyphicon glyphicon-off" style = "color:black"></i><h7 style = "color:black">Cerrar Sesión</h7></a></li>' + '</form>' + '</div>' + '</li>';
-    return buttons;
-}
-
-function showError(error) {
-    alert(error.message);
-}
-
-function signIn() {
-    var request = new Object();
-    request.url = "http://eiffel.itba.edu.ar/hci/service3/Account.groovy?method=SignIn&username=" + document.getElementById('loginUser').value + "&password=" + document.getElementById('loginPassword').value;
-    request.dataType = "jsonp";
-    console.log(request.url);
-    $.ajax(request).done(function(data) {
-        error = data.error;
-        if (error === undefined) {
-            localStorage.setItem("loggedUser", JSON.stringify(data));
-            location.reload();
-        } else {
-            showError(error);
-        }
-    });
-
-}
-
-function deleteAccount() {
-    sessionStorage.setItem("account", null);
-    location.reload();
-}
-
-function signOut() {
-    var account = JSON.parse(sessionStorage.getItem("loggedUser"));
-    var token = sessionStorage.getItem("token");
-
-    var request = new Object();
-    request.url = "http://eiffel.itba.edu.ar/hci/service3/Account.groovy?method=SignOut&username=" + account.username + "&authentication_token=" + token;
-    request.dataType = "jsonp";
-
-    $.ajax(request).done(function(data) {
-        handleResponse(data, deleteAccount);
-    });
-}
-
-
-function CreateAccount() {
-
-   var username = document.getElementById("usernameP").value;
-	var firstname = document.getElementById("nombre").value;
-	var dni = document.getElementById("dni").value;
-	var password = document.getElementById("passwordP").value;
-	var email = document.getElementById("emailP").value;
-	var lastName = document.getElementById("apellido").value;
-	var birthdate = "1989-04-10"; 
-	var gender = "M";
-
-    var account2 = {
-        "username": username,
-        "password": password,
-        "firstName": firstname,
-        "lastName": lastName,
-        "gender": gender,
-        "identityCard": dni,
-        "email": email,
-        "birthDate": birthdate
-    };
-
-
-    var request = new Object();
-    request.url = "http://eiffel.itba.edu.ar/hci/service3/Account.groovy?method=CreateAccount&account=" + encodeURIComponent(JSON.stringify(account2)); 
-    	request.dataType = "jsonp"; 
-    	console.log(password); 
-    	console.log(username); 
-    	console.log(firstname); 
-    	console.log(request.url);
-    	 $.ajax(request).done(function(data) {
-            console.log(data);
-            error = data.error;
-            if (error === undefined) {
-            	console.log(password); 
-    			console.log(username); 
-    			console.log(firstname); 
-    			console.log(request.url);
-            	localStorage.setItem("loggedUser", JSON.stringify(data));
-                localStorage.setItem("account", JSON.stringify(data));
-                //location.reload();
-            } else {
-                showError(error);
-            }
+    if (notSignedIn()) {
+        $("#navbarContent").append(notLoggedInButtons());           // me parece que navbarContent no existe en pagPrinipal.html
+        document.getElementById("sign-in").addEventListener("click", function() {
+            signIn()
         });
-   
-    	
-    
-    
-            //location.reload();
-        
-    
-    	/*request.dataType = "jsonp"; 
-    	console.log(password); 
-    	console.log(username); 
-    	console.log(firstname); 
-    	console.log(request.url);
-    	 $.ajax(request).done(function(data) {
-            console.log(data);
+        document.getElementById("registrarse").addEventListener("click", function() {
+            console.log('entrooo');
+            $scope.CreateAccount();  // el scope permite encontrar la función
+        });
+    } else {
+        document.getElementById("cerrarSesion").addEventListener("click", function() {
+            signOut()
+        });
+        $("#navbarContent").append(loggedInButtons());
+    }
+
+
+
+    function notSignedIn() {
+        var loggedUser = sessionStorage.getItem("loggedUser");
+        if (loggedUser == null || loggedUser == "" || loggedUser === "undefined" || loggedUser == "null" || loggedUser === undefined) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function notLoggedInButtons() {
+        var buttons = '<li><a href="#" data-toggle="modal" data-target="#myModal" >Registrarse</a></li>' + '<li class="divider-vertical"></li>' + '<li class="dropdown">' + '<a class="dropdown-toggle" href="#" data-toggle="dropdown">Iniciar Sesión<strong class="caret"></strong></a>' + '<div class="dropdown-menu" style="padding: 15px; padding-bottom: 9px;">' + '<form method="post"  accept-charset="UTF-8">' + '<input style="margin-bottom: 15px;" type="text" placeholder="Usuario" id="loginUser" name="username">' + '<input style="margin-bottom: 15px;" type="password" placeholder="Contraseña" id="loginPassword" name="password">' + '<input class="btn btn-primary btn-block" id="sign-in" value="Iniciar Sesión">' + '<a data-toggle="modal" data-target="#cont" href="#"><span class="olvidoC">¿Olvidaste tu contraseña?</span></a>' + '</form>' + '</div>' + '</li>';
+        return buttons;
+    }
+
+    function loggedInButtons() {
+        var buttons = ' <li class="dropdown">' + '<a class="dropdown-toggle" href="#" data-toggle="dropdown">Mi Cuenta<strong class="caret"></strong></a>' + '<div class="dropdown-menu" style="padding: 15px; padding-bottom: 9px;">' + '<form method="post" action="login" accept-charset="UTF-8">' + '<li><a href="usuario.html"><i class="glyphicon glyphicon-user" style = "color:black"></i><h7 style = "color:black">Mi Usuario</h7></a></li>' + '<li><a href="contacto.html"><i class="glyphicon glyphicon-envelope" style = "color:black"></i><h7 style = "color:black"> Contacto</h7></a></li>' + '<li class="divider"></li>' + '<li><a href="#" id="cerrarSesion"><i class="glyphicon glyphicon-off" style = "color:black"></i><h7 style = "color:black">Cerrar Sesión</h7></a></li>' + '</form>' + '</div>' + '</li>';
+        return buttons;
+    }
+
+    function showError(error) {
+        alert(error.message);
+    }
+
+    function signIn() {
+        var request = new Object();
+        request.url = "http://eiffel.itba.edu.ar/hci/service3/Account.groovy?method=SignIn&username=" + document.getElementById('loginUser').value + "&password=" + document.getElementById('loginPassword').value;
+        request.dataType = "jsonp";
+        console.log(request.url);
+        $.ajax(request).done(function(data) {
             error = data.error;
             if (error === undefined) {
-            	sessionStorage.setItem("account", JSON.stringify(data));
+                localStorage.setItem("loggedUser", JSON.stringify(data));
                 location.reload();
             } else {
                 showError(error);
             }
-        });*/
+        });
 
     }
+
+    function deleteAccount() {
+        sessionStorage.setItem("account", null);
+        location.reload();
+    }
+
+    function signOut() {
+        var account = JSON.parse(sessionStorage.getItem("loggedUser"));
+        var token = sessionStorage.getItem("token");
+
+        var request = new Object();
+        request.url = "http://eiffel.itba.edu.ar/hci/service3/Account.groovy?method=SignOut&username=" + account.username + "&authentication_token=" + token;
+        request.dataType = "jsonp";
+
+        $.ajax(request).done(function(data) {
+            handleResponse(data, deleteAccount());
+        });
+    }
+
+
+    $scope.CreateAccount = function() {         // el scope es para que encuentre a la funcion
+
+        var username = document.getElementById("usernameP").value;
+        var firstname = document.getElementById("nombre").value;
+        var dni = document.getElementById("dni").value;
+        var password = document.getElementById("passwordP").value;
+        var email = document.getElementById("emailP").value;
+        var lastName = document.getElementById("apellido").value;
+        var birthdate = "1989-04-10";       // los hice manualmente para no perder tiempo
+        var gender = "M";
+
+        var account2 = {            // creo el objeto account
+            "username": username,
+            "password": password,
+            "firstName": firstname,
+            "lastName": lastName,
+            "gender": gender,
+            "identityCard": dni,
+            "email": email,
+            "birthDate": birthdate
+        };
+
+        // aca estuve probando mediante dos metodos para llamar createAccount
+
+
+        /*
+        var request = new Object();
+        request.url = "http://eiffel.itba.edu.ar/hci/service3/Account.groovy?method=CreateAccount&account=" + encodeURIComponent(JSON.stringify(account2));
+        console.log(request);
+
+        $.ajax(request).done(function(data) {
+            console.log(data);
+            handleResponse(data, localStorage.setItem("loggedUser", JSON.stringify(data)));
+
+        });*/
+        
+        // el problema aca es que esta entrando a la segunda funcion que es para el manejo de errorres, es decir no esta funcionando
+
+        $http({
+            method: 'GET',
+            url: "http://eiffel.itba.edu.ar/hci/service3/Account.groovy?method=CreateAccount&account=" + encodeURIComponent(JSON.stringify(account2))
+        }).then(function(response) {                    // entra en esta funcion si no hubo ningun error
+            // console.log("response:" + response);
+            if (response.data.error) {
+                $scope.handleError(response.data.error.message);
+            } else {
+                //console.log(response.data);
+                $scope.example4 = response;
+            }
+        }, function(data) {                 // esta es la segunda funcion a la cual esta entrando
+            if (data.status == 0) {
+                $scope.handleError("Request timed out");
+            } else {
+                $scope.handleError(data.status + " - " + data);
+            }
+        });
+
+
+
+    }
+
+}); // cierra el controller
+
+
+
+
--- js/listaOfertas.js
+++ js/listaOfertas.js
@@ -1,73 +1,72 @@
-
 localStorage.removeItem("carrito");
 
 var carro = JSON.parse(localStorage.getItem("carrito"));
-if(carro == undefined){
-	alert("creo carrito");
-	var carrito = [];
+if (carro == undefined) {
+    alert("creo carrito");
+    var carrito = [];
 
-	localStorage.setItem("carrito", JSON.stringify(carrito));
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
 
 var request = new Object();
-request.url="http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetAllProducts&id=1&page_size=18";
+request.url = "http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetAllProducts&id=1&page_size=18";
 
 
 
 
-request.dataType="jsonp";
+request.dataType = "jsonp";
 console.log(request.url);
-$.ajax(request).done( function(data) {
-		sessionStorage.setItem("ofertas",JSON.stringify(data));
-		addOffers();
+$.ajax(request).done(function(data) {
+    sessionStorage.setItem("ofertas", JSON.stringify(data));
+    addOffers();
 });
 
 
 
-function addOffers(){
+function addOffers() {
 
-	document.getElementById('ofertas').innerHTML = "";
+    document.getElementById('ofertas').innerHTML = "";
 
 
-	var data = JSON.parse(sessionStorage.getItem("ofertas"));
+    var data = JSON.parse(sessionStorage.getItem("ofertas"));
 
-	var i  = 0;
-	for (i = 0; i < data.total && data.products[i] != undefined; i++) { 
-		if(i % 6 == 0){
+    var i = 0;
+    for (i = 0; i < data.total && data.products[i] != undefined; i++) {
+        if (i % 6 == 0) {
 
-		document.getElementById('ofertas').innerHTML += '<div class="container">'+
-														'<div class="row">';
-		}
+            document.getElementById('ofertas').innerHTML += '<div class="container">' +
+                '<div class="row">';
+        }
 
-		var ID = "pagProd.html?product" + "=" + data.products[i].id;
+        var ID = "pagProd.html?product" + "=" + data.products[i].id;
 
-		var prod = '<div class="col-md-2 col-sm-6 col-xs-6">'+
-				'<a onclick="loadProduct(data.products[i].name)" href='+ID+'>'+
-					'<div class="panel panel-default">'+
-						'<div class="panel-body">'+
-							'<div class="imgWrapper">'+
-								'<img src='+data.products[i].imageUrl[0]+'>'+
-							'</div>'+
-							'<br>'+
-							'<font size="3" style="color:black"><b>'+data.products[i].name+'</b></font>'+
-							'<br>'+
-							'<font size="2" style="color:grey">$'+data.products[i].price+'</font>'+
-						'</div>'+
-					'</div>'+
-				'</a>'+
-			'</div>';
+        var prod = '<div class="col-md-2 col-sm-6 col-xs-6">' +
+            '<a onclick="loadProduct(data.products[i].name)" href=' + ID + '>' +
+            '<div class="panel panel-default">' +
+            '<div class="panel-body">' +
+            '<div class="imgWrapper">' +
+            '<img src=' + data.products[i].imageUrl[0] + '>' +
+            '</div>' +
+            '<br>' +
+            '<font size="3" style="color:black"><b>' + data.products[i].name + '</b></font>' +
+            '<br>' +
+            '<font size="2" style="color:grey">$' + data.products[i].price + '</font>' +
+            '</div>' +
+            '</div>' +
+            '</a>' +
+            '</div>';
 
-		document.getElementById('ofertas').innerHTML += prod;
+        document.getElementById('ofertas').innerHTML += prod;
 
-		if(i % 6 == 5){
+        if (i % 6 == 5) {
 
-		document.getElementById('ofertas').innerHTML += '</div>'+
-														'</div>'+
-														'<br>'+
-														'<br>'+
-														'<br>';
-		}
-	}
+            document.getElementById('ofertas').innerHTML += '</div>' +
+                '</div>' +
+                '<br>' +
+                '<br>' +
+                '<br>';
+        }
+    }
 
 }
