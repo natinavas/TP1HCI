177c12e13c9fadc874e6892842c269a015356393
natinavas <nnavas@itba.edu.ar>
2015-10-14 20:41:01 -0300

    sin merge conflict etc

--- js/navBarMenus.js
+++ js/navBarMenus.js
@@ -1,35 +1,3 @@
-
-/*var request = new Object();
-request.url = "http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetAllSubcategories&id=1";
-request.dataType = "jsonp";
-
-console.log(request.url);
-
-$.ajax(request).done(function(calzado) {
-	error=calzado.error;
-    if (error === undefined) {
-		alert(calzado.subcategories[0].attributes[0].values[0]);
-		var i;
-		for(i=0; calzado.subcategorioes[i] != undefined; i++){
-			if(calzado.subcategories[i].attributes[0].values[0] == "Adulto" && calzado.category.attributes[1].values[i] == "Femenino"){
-				document.getElementById('calzadoMujeres').innerHTML = '<li><a href="#">Alpargatas</a></li>';
-				
-			}
-	}			
-				
-        document.getElementById('navBarMujeres').innerHTML += "";
-    } else {
-        showError(error);
-    }
-	
-});
-
-function showError(error) {
-    alert(error.message);
-<<<<<<< HEAD
-}
-*/
-
 loadCalzadoFem();
 loadIndumentariaFem();
 loadAccesoriosFem();
@@ -88,6 +56,3 @@ function loadNavBarFMClothes(num, categ, gender){
 		}		
 	});
 }
-=======
-}*/
->>>>>>> origin/master
