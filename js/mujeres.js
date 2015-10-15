3dd9af09e15824dadb999df7d11df528e391e5ca
fbartolome <fbartolome18@gmail.com>
2015-10-15 11:06:23 -0300

    estuve haciendo la pagina de mujeres dinamicamente

--- js/mujeres.js
+++ js/mujeres.js
@@ -21,3 +21,101 @@ $(document).ready( function() {
 		clickEvent = false;
 	});
 });
+
+loadIndumentaria();
+loadCalzado();
+
+function loadIndumentaria(){
+
+	var request = new Object();
+	request.url = "http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetAllSubcategories&id=2&filters=[{%20%22id%22:%201,%20%22value%22:%20%22Femenino%22%20}]";
+	request.dataType = "jsonp";
+
+	console.log(request.url);
+
+	$.ajax(request).done(function(c){
+		var i;
+		for(i = 0; i < c.subcategories.length; i++){
+			if(i%6 == 0){
+				if(i != 0){
+					$("#indumentaria").append('</div>');
+				}
+				$("#indumentaria").append('<div class="row">');
+			}
+			$("#indumentaria").append('<div class="col-xs-2">'+
+		                    '<div class="row">'+
+		                        '<div class=text-center>'+
+		                            '<h3>'+ c.subcategories[i].name +'</h3></div>'+
+		                    '</div>'+
+		                    '<a href="pagEnConstruccion.html">'+
+		                        '<div class="bw pic">'+
+		                            '<img src="img/menuMujeres/'+ c.subcategories[i].name +'.jpg" title="'+ c.subcategories[i].name +'" alt="'+ c.subcategories[i].name +'" style="width:200px;height:275px;">'+
+		                        '</div>'+
+		                    '</a>'+
+		                '</div>');	
+		}
+
+		$("#indumentaria").append('<div class="col-xs-2">'+
+		                    '<div class="row">'+
+		                        '<div class=text-center>'+
+		                            '<h3>Todo Indumentaria</h3></div>'+
+		                    '</div>'+
+		                    '<a href="pagEnConstruccion.html">'+
+		                        '<div class="bw pic">'+
+		                            '<img src="img/menuMujeres/vertodoIndumentaria.jpg" title="vertodoIndumentaria" alt="vertodoIndumentaria" style="width:200px;height:275px;">'+
+		                        '</div>'+
+		                    '</a>'+
+		                '</div>');	
+
+		$("#indumentaria").append('</div>');		
+
+	});
+}
+
+
+function loadCalzado(){
+
+	var request = new Object();
+	request.url = "http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetAllSubcategories&id=1&filters=[{%20%22id%22:%201,%20%22value%22:%20%22Femenino%22%20}]";
+	request.dataType = "jsonp";
+
+	console.log(request.url);
+
+	$.ajax(request).done(function(c){
+		var i;
+		for(i = 0; i < c.subcategories.length; i++){
+			if(i%6 == 0){
+				if(i != 0){
+					$("#calzado").append('</div>');
+				}
+				$("#calzado").append('<div class="row">');
+			}
+			$("#calzado").append('<div class="col-xs-2">'+
+		                    '<div class="row">'+
+		                        '<div class=text-center>'+
+		                            '<h3>'+ c.subcategories[i].name +'</h3></div>'+
+		                    '</div>'+
+		                    '<a href="pagEnConstruccion.html">'+
+		                        '<div class="bw pic">'+
+		                            '<img src="img/menuMujeres/'+ c.subcategories[i].name +'.jpg" title="'+ c.subcategories[i].name +'" alt="'+ c.subcategories[i].name +'" style="width:200px;height:275px;">'+
+		                        '</div>'+
+		                    '</a>'+
+		                '</div>');	
+		}
+
+		$("#calzado").append('<div class="col-xs-2">'+
+		                    '<div class="row">'+
+		                        '<div class=text-center>'+
+		                            '<h3>Todo Calzado</h3></div>'+
+		                    '</div>'+
+		                    '<a href="pagEnConstruccion.html">'+
+		                        '<div class="bw pic">'+
+		                            '<img src="img/menuMujeres/vertodoCalzado.jpg" title="vertodoIndumentaria" alt="vertodoCalzado" style="width:200px;height:275px;">'+
+		                        '</div>'+
+		                    '</a>'+
+		                '</div>');	
+
+		$("#calzado").append('</div>');		
+
+	});
+}
