4e47bdad88f5f767725d5b81afa67eab16a988f6
Jose Angel Vitali <jovitali@lab67.it.itba.edu.ar>
2015-10-15 20:59:38 -0300

    cart piola

--- js/cart.js
+++ js/cart.js
@@ -17,6 +17,14 @@ else{
 }
 
 for(i = 0; cart[i] != undefined; i++){
+	loadProduct(i);
+}
+
+
+function loadProduct(i){
+
+
+	var cart = JSON.parse(localStorage.getItem("carrito"));
 
 	var color = JSON.parse(cart[i]).color;
 	var talle = JSON.parse(cart[i]).talle;
@@ -32,46 +40,73 @@ for(i = 0; cart[i] != undefined; i++){
 	costoEnvio += Math.round(price/20);
 	total += price + Math.round(price/20);
 
-
-	var prod = 	'<div class="row">'+
-	            '<div class="col-xs-2"><img class="img-responsive" src=' + image + '>'+
-	            '</div>'+
-	            '<div class="col-xs-4">'+
-	                '<h4 class="product-name"><strong>'+ name + '</strong></h4>'+
-	                '<h4>Marca: '+ marca + '</h4>'+
-	                
-	            '</div>'+
-	            '<div class="col-xs-6">'+
-	                '<div class="col-xs-6 text-right">'+
-	                    '<h4><strong>$' + price + ' <span class="text-muted">x</span></strong></h4>'+
-	                '</div>'+
-	                '<div class="col-xs-4">'+
-	                    '<input type="number" name="quantity" min="1" max="15" value="' + quantity + '">'+
-	                '</div>'+
-	                '<div class="col-xs-2">'+
-	                    '<button type="button" class="btn btn-link btn-xs">'+
-	                        '<span class="glyphicon glyphicon-trash" style="color:black"> </span>'+
-	                    '</button>'+
-	                    '<button type="button" class="btn btn-link btn-xs">'+
-	                '<span class="glyphicon glyphicon-heart-empty" style="color:black"></span>'+
-	                    '</button>'+
-	                '</div>'+
-	            '</div>'+
-	            '<br>'+
-	            '<br>'+
-	            '<div class="col-xs-3 col-xs-offset-1">'+
-	            '<h4>Color: ' + color + '</h4>' +
-	            '<h4>Talle: ' + talle + '</h4>' +
-	            '</div><br/><br/><br/>'+
-	        '</div>'+
-	        '<hr></hr>';
+	var number = i;
 
 
+	var prod = 	'<div id="' + number + '">'+
+					'<div class="row">'+
+		            	'<div class="col-xs-2"><img class="img-responsive" src=' + image + '>'+
+		            	'</div>'+
+		            	'<div class="col-xs-4">'+
+		                	'<h4 class="product-name"><strong>'+ name + '</strong></h4>'+
+		                	'<h4>Marca: '+ marca + '</h4>'+
+		            	'</div>'+
+		            	'<div class="col-xs-6">'+
+			                '<div class="col-xs-6 text-right">'+
+			                    '<h4><strong>$' + price + ' <span class="text-muted">x</span></strong></h4>'+
+			                '</div>'+
+		                '<div class="col-xs-4">'+
+		                    '<input type="number" name="quantity" min="1" max="15" value="' + quantity + '">'+
+		                '</div>'+
+		                '<div class="col-xs-2">'+
+		                    '<button type="button" id="trash' + number + '" class="btn btn-link btn-xs">'+
+		                        '<span class="glyphicon glyphicon-trash" style="color:black"> </span>'+
+		                    '</button>'+
+		                    '<button type="button" id="fav' + number + '" onclick="hola()" class="btn btn-link btn-xs">'+
+		                		'<span class="glyphicon glyphicon-heart-empty" style="color:black"></span>'+
+		                    '</button>'+
+		                '</div>'+
+		            '</div>'+
+		            '<br/>'+
+		            '<br/>'+
+		            '<div class="col-xs-3 col-xs-offset-1">'+
+			            '<h4>Color: ' + color + '</h4>' +
+			            '<h4>Talle: ' + talle + '</h4>' +
+		            '</div><br/><br/><br/>'+
+		        	'<hr></hr>'+
+	        	'</div>';
 
 	        document.getElementById("cartProducts").innerHTML += prod;
+
+
+	        $(document).on('click', "#trash"+number, function(){
+	        	removeItem(number);
+	        });
+	        $(document).on('click', "#fav"+number, function(){
+	        	addItemToFav(number);
+	        });
 
 document.getElementById("subtotal").innerHTML = "$" + subtotal;
 document.getElementById("costoEnvio").innerHTML = "$" + costoEnvio;
 document.getElementById("total").innerHTML = "$" + total;
+
+}
+
+
+function removeItem(number){
+	$("#"+number).remove();
+	var cart = JSON.parse(localStorage.getItem("carrito"));
+	cart[number]
+
+	if (number > -1) {
+    	cart.splice(number, 1);
+	}
+
+	localStorage.setItem("carrito", JSON.stringify(cart));
+
+}
+
+function addItemToFav(number){
+	alert(number);
 
 }
